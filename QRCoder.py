2cbb4c53d7d3fef956a6663ab6a3e7ef093063c9
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2021-05-11 17:21:45 -0700

    Working

--- QRCoder.py
+++ QRCoder.py
@@ -6,108 +6,41 @@ import os
 try:
     from . import config
     from .apper import apper
+    from .commands.QRCodeMaker import QRCodeMaker
 
-    # ************Samples**************
-    # Basic Fusion 360 Command Base samples
-    from .commands.SampleCommand1 import SampleCommand1
-    from .commands.SampleCommand2 import SampleCommand2
-
-    # Palette Command Base samples
-    from .commands.SamplePaletteCommand import SamplePaletteSendCommand, SamplePaletteShowCommand
-
-    # Various Application event samples
-    from .commands.SampleCustomEvent import SampleCustomEvent
-    from .commands.SampleDocumentEvents import SampleDocumentEvent1, SampleDocumentEvent2
-    from .commands.SampleWorkspaceEvents import SampleWorkspaceEvent
-    from .commands.SampleWebRequestEvent import SampleWebRequestOpened
-    from .commands.SampleCommandEvents import SampleCommandEvent
-    from .commands.SampleActiveSelectionEvents import SampleActiveSelectionEvent
-
-    # Create our addin definition object
     my_addin = apper.FusionApp(config.app_name, config.company_name, False)
     my_addin.root_path = config.app_path
 
-    # Creates a basic Hello World message box on execute
-    my_addin.add_command(
-        'Sample Command 1',
-        SampleCommand1,
-        {
-            'cmd_description': 'Hello World!',
-            'cmd_id': 'sample_cmd_1',
-            'workspace': 'FusionSolidEnvironment',
-            'toolbar_panel_id': 'Commands',
-            'cmd_resources': 'command_icons',
-            'command_visible': True,
-            'command_promoted': True,
-        }
-    )
-
     # General command showing inputs and user interaction
     my_addin.add_command(
-        'Sample Command 2',
-        SampleCommand2,
+        'Create QR Code',
+        QRCodeMaker,
         {
-            'cmd_description': 'A simple example of a Fusion 360 Command with various inputs',
-            'cmd_id': 'sample_cmd_2',
+            'cmd_description': 'Generate QR Code geometry by encoding a message with various options.',
+            'cmd_id': 'make_qr',
             'workspace': 'FusionSolidEnvironment',
             'toolbar_panel_id': 'Commands',
-            'cmd_resources': 'command_icons',
-            'command_visible': True,
-            'command_promoted': False,
-        }
-    )
-
-    # Create an html palette to as an alternative UI
-    my_addin.add_command(
-        'Sample Palette Command - Show',
-        SamplePaletteShowCommand,
-        {
-            'cmd_description': 'Shows the Fusion 360 Demo Palette',
-            'cmd_id': 'sample_palette_show',
-            'workspace': 'FusionSolidEnvironment',
-            'toolbar_panel_id': 'Palette',
-            'cmd_resources': 'palette_icons',
+            'cmd_resources': 'make_qr_icons',
             'command_visible': True,
             'command_promoted': True,
-            'palette_id': 'sample_palette',
-            'palette_name': 'Sample Fusion 360 HTML Palette',
-            'palette_html_file_url': os.path.join('commands', 'palette_html', 'QRCoder.html'),
-            'palette_use_new_browser': True,
-            'palette_is_visible': True,
-            'palette_show_close_button': True,
-            'palette_is_resizable': True,
-            'palette_width': 500,
-            'palette_height': 600,
+            'is_make_qr': True
         }
     )
 
-    # Send data from Fusion 360 to the palette
     my_addin.add_command(
-        'Send Info to Palette',
-        SamplePaletteSendCommand,
+        'Import QR Code',
+        QRCodeMaker,
         {
-            'cmd_description': 'Send data from a regular Fusion 360 command to a palette',
-            'cmd_id': 'sample_palette_send',
+            'cmd_description': 'Generate QR Code geometry by importing definition from a csv file.',
+            'cmd_id': 'import_qr',
             'workspace': 'FusionSolidEnvironment',
-            'toolbar_panel_id': 'Palette',
-            'cmd_resources': 'palette_icons',
+            'toolbar_panel_id': 'Commands',
+            'cmd_resources': 'csv_qr_icons',
             'command_visible': True,
-            'command_promoted': False,
-            'palette_id': 'sample_palette',
+            'command_promoted': True,
+            'is_make_qr': False
         }
     )
-
-    app = adsk.core.Application.cast(adsk.core.Application.get())
-    ui = app.userInterface
-
-    # Uncomment as necessary.  Running all at once can be overwhelming :)
-    # my_addin.add_custom_event("QRCoder_message_system", SampleCustomEvent)
-    # my_addin.add_document_event("QRCoder_open_event", app.documentActivated, SampleDocumentEvent1)
-    # my_addin.add_document_event("QRCoder_close_event", app.documentClosed, SampleDocumentEvent2)
-    # my_addin.add_workspace_event("QRCoder_workspace_event", ui.workspaceActivated, SampleWorkspaceEvent)
-    # my_addin.add_web_request_event("QRCoder_web_request_event", app.openedFromURL, SampleWebRequestOpened)
-    # my_addin.add_command_event("QRCoder_command_event", app.userInterface.commandStarting, SampleCommandEvent)
-    # my_addin.add_command_event("QRCoder_active_selection_event", ui.activeSelectionChanged, SampleActiveSelectionEvent)
 
 except:
     app = adsk.core.Application.get()
---
+++ commands/QRCodeMaker.py
@@ -0,0 +1,269 @@
+import csv
+import os.path
+
+import adsk.core
+import adsk.fusion
+import adsk.cam
+
+from ..apper import apper
+from .. import config
+
+# Defaults
+BLOCK = '.5 in'
+HEIGHT = '.25 in'
+BASE = '.25 in'
+MESSAGE = 'https://www.autodesk.com/products/fusion-360/overview'
+
+# File assumed to be in script root directory
+FILE_NAME = 'QR-17x.csv'
+
+
+def make_real_geometry(component: adsk.fusion.Component, t_body: adsk.fusion.BRepBody):
+    base_feature = component.features.baseFeatures.add()
+    base_feature.startEdit()
+    component.bRepBodies.add(t_body, base_feature)
+    base_feature.finishEdit()
+
+    tools = adsk.core.ObjectCollection.create()
+    tools.add(base_feature.bodies.item(0))
+    combine_input = component.features.combineFeatures.createInput(component.bRepBodies.item(0), tools)
+    component.features.combineFeatures.add(combine_input)
+
+
+def clear_graphics(graphics_group: adsk.fusion.CustomGraphicsGroup):
+    for entity in graphics_group:
+        if entity.isValid:
+            entity.deleteMe()
+
+
+def make_graphics(t_body: adsk.fusion.BRepBody, graphics_group: adsk.fusion.CustomGraphicsGroup):
+    clear_graphics(graphics_group)
+    color = adsk.core.Color.create(250, 162, 27, 255)
+    color_effect = adsk.fusion.CustomGraphicsSolidColorEffect.create(color)
+    graphics_body = graphics_group.addBRepBody(t_body)
+    graphics_body.color = color_effect
+
+
+def get_qr_temp_geometry(qr_data, input_values):
+    side: float = input_values['block_size']
+    height: float = input_values['block_height']
+    base: float = input_values['base_height']
+    sketch_point: adsk.fusion.SketchPoint = input_values['sketch_point'][0]
+
+    x_dir = sketch_point.parentSketch.xDirection
+    x_dir.normalize()
+    y_dir = sketch_point.parentSketch.yDirection
+    y_dir.normalize()
+
+    qr_size = len(qr_data)
+    middle_point = sketch_point.worldGeometry
+    start_point = middle_point.copy()
+    x_start_move = x_dir.copy()
+    x_start_move.scaleBy((.5 * side) * (1 - qr_size))
+    start_point.translateBy(x_start_move)
+    y_start_move = y_dir.copy()
+    y_start_move.scaleBy((.5 * side) * (qr_size - 1))
+    start_point.translateBy(y_start_move)
+
+    b_mgr = adsk.fusion.TemporaryBRepManager.get()
+    full_size = side * qr_size
+    base_t_box = adsk.core.OrientedBoundingBox3D.create(middle_point, x_dir, y_dir, full_size, full_size, base)
+    base_t_body = b_mgr.createBox(base_t_box)
+
+    for i, row in enumerate(qr_data):
+        for j, col in enumerate(row):
+            if int(col) == 1:
+                x_move = x_dir.copy()
+                y_move = y_dir.copy()
+                x_move.scaleBy(j * side)
+                y_move.scaleBy(-1 * i * side)
+
+                c_point = start_point.copy()
+                c_point.translateBy(x_move)
+                c_point.translateBy(y_move)
+
+                b_box = adsk.core.OrientedBoundingBox3D.create(c_point, x_dir, y_dir, side, side, height + base)
+                t_body = b_mgr.createBox(b_box)
+                b_mgr.booleanOperation(base_t_body, t_body, adsk.fusion.BooleanTypes.UnionBooleanType)
+
+    return base_t_body
+
+
+def import_qr_from_file(file_name):
+    qr_data = []
+
+    if os.path.exists(file_name):
+        with open(file_name, newline='') as f:
+            reader = csv.reader(f)
+            qr_data = list(reader)
+
+    return qr_data
+
+
+@apper.lib_import(config.lib_path)
+def build_qr_code(message, args):
+    import pyqrcode
+    try:
+        qr = pyqrcode.create(message, **args)
+        qr_text = qr.text(quiet_zone=0)
+        qr_data = [[char for char in y] for y in (x.strip() for x in qr_text.splitlines()) if y]
+        return qr_data
+
+    except ValueError as e:
+        ao = apper.AppObjects()
+        ao.ui.messageBox(f'Problem with inputs: {e}')
+        return []
+
+
+def make_qr_from_message(input_values):
+    message: str = input_values['message']
+    use_user_size: bool = input_values['use_user_size']
+    user_size: int = input_values['user_size']
+    mode: str = input_values['mode']
+    error_type: str = input_values['error_type']
+
+    args = {}
+    if use_user_size:
+        args['version'] = user_size
+    if mode != 'Automatic':
+        args['mode'] = mode
+    if error_type != 'Automatic':
+        args['error'] = error_type
+
+    qr_data = build_qr_code(message, args)
+    return qr_data
+
+
+def add_make_inputs(inputs: adsk.core.CommandInputs):
+    drop_style = adsk.core.DropDownStyles.TextListDropDownStyle
+    inputs.addStringValueInput('message', 'Value to encode', MESSAGE)
+
+    inputs.addBoolValueInput('use_user_size', 'Specify size?', True, '', False)
+    size_spinner = inputs.addIntegerSpinnerCommandInput('user_size', 'QR Code Size (Version)', 1, 40, 1, 5)
+    size_spinner.isEnabled = False
+    size_spinner.isVisible = False
+
+    mode_drop_down = inputs.addDropDownCommandInput('mode', 'Encoding Mode', drop_style)
+    mode_items = mode_drop_down.listItems
+    mode_items.add('Automatic', True, '')
+    mode_items.add('alphanumeric', False, '')
+    mode_items.add('numeric', False, '')
+    mode_items.add('binary', False, '')
+    mode_items.add('kanji', False, '')
+
+    error_input = inputs.addDropDownCommandInput('error_type', 'Encoding Mode', drop_style)
+    error_items = error_input.listItems
+    error_items.add('Automatic', True, '')
+    error_items.add('L', False, '')
+    error_items.add('M', False, '')
+    error_items.add('Q', False, '')
+    error_items.add('H', False, '')
+
+
+def add_csv_inputs(inputs: adsk.core.CommandInputs):
+    default_file_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), FILE_NAME)
+    inputs.addStringValueInput('file_name', "File to import", '')
+
+    browse_button = inputs.addBoolValueInput('browse', 'Browse', False, '', False)
+    browse_button.isFullWidth = True
+
+
+# Create file browser dialog box
+def browse_for_csv():
+    ao = apper.AppObjects()
+
+    file_dialog = ao.ui.createFileDialog()
+    file_dialog.initialDirectory = config.app_path
+    file_dialog.filter = ".csv files (*.csv)"
+    file_dialog.isMultiSelectEnabled = False
+    file_dialog.title = 'Select csv file to import'
+    dialog_results = file_dialog.showOpen()
+
+    if dialog_results == adsk.core.DialogResults.DialogOK:
+        file_names = file_dialog.filenames
+        return file_names[0]
+    else:
+        return ''
+
+
+class QRCodeMaker(apper.Fusion360CommandBase):
+    def __init__(self, name: str, options: dict):
+        super().__init__(name, options)
+        # self.graphics_group = None
+        self.make_preview = True
+        self.is_make_qr = options.get('is_make_qr', False)
+
+    def on_input_changed(self, command, inputs, changed_input, input_values):
+        self.make_preview = True
+        if changed_input.id == 'use_user_size':
+            if input_values['use_user_size']:
+                inputs.itemById('user_size').isEnabled = True
+                inputs.itemById('user_size').isVisible = True
+            else:
+                inputs.itemById('user_size').isEnabled = False
+                inputs.itemById('user_size').isVisible = False
+        elif changed_input.id == 'browse':
+            changed_input.value = False
+            file_name = browse_for_csv()
+            if len(file_name) > 0:
+                inputs.itemById('file_name').value = file_name
+
+    def on_preview(self, command, inputs, args, input_values):
+        if self.make_preview:
+            qr_data = []
+            if self.is_make_qr:
+                qr_data = make_qr_from_message(input_values)
+
+            else:
+                file_name = input_values['file_name']
+                if len(file_name) > 0:
+                    qr_data = import_qr_from_file(file_name)
+
+            if len(qr_data) > 0:
+                t_body = get_qr_temp_geometry(qr_data, input_values)
+
+                # TODO maybe this should be an option?
+                sketch_point: adsk.fusion.SketchPoint = input_values['sketch_point'][0]
+                target_component = sketch_point.parentSketch.parentComponent
+
+                # TODO Why is custom graphics so slow?
+                make_real_geometry(target_component, t_body)
+                # make_graphics(t_body, self.graphics_group)
+                args.isValidResult = True
+
+            self.make_preview = False
+
+    def on_execute(self, command, inputs, args, input_values):
+        # sketch_point: adsk.fusion.SketchPoint = input_values['sketch_point'][0]
+        # t_body = build_qr(input_values)
+        # make_real_geometry(sketch_point.parentSketch.parentComponent, t_body)
+        pass
+
+    def on_destroy(self, command, inputs, reason, input_values):
+        # clear_graphics(self.graphics_group)
+        # self.graphics_group.deleteMe()
+        pass
+
+    def on_create(self, command, inputs):
+        ao = apper.AppObjects()
+        # self.graphics_group = ao.root_comp.customGraphicsGroups.add()
+        self.make_preview = True
+
+        default_block_size = adsk.core.ValueInput.createByString(BLOCK)
+        default_block_height = adsk.core.ValueInput.createByString(HEIGHT)
+        default_base_height = adsk.core.ValueInput.createByString(BASE)
+        default_units = ao.units_manager.defaultLengthUnits
+
+        selection_input = inputs.addSelectionInput('sketch_point', "Center Point", "Pick Sketch Point for center")
+        selection_input.addSelectionFilter("SketchPoints")
+
+        inputs.addValueInput('block_size', 'QR Block Size', default_units, default_block_size)
+        inputs.addValueInput('block_height', 'QR Block Height', default_units, default_block_height)
+        inputs.addValueInput('base_height', 'Base Height', default_units, default_base_height)
+
+        group_input = inputs.addGroupCommandInput('group', 'QR Code Definition')
+
+        if self.is_make_qr:
+            add_make_inputs(group_input.children)
+        else:
+            add_csv_inputs(group_input.children)
